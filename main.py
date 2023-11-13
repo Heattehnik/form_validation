22d99e2c273467d76d661d07b669b9ce1a98446c
Heattehnik <roman.ecm@mail.ru>
2023-11-13 22:57:51 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from tinydb import TinyDB
+from fastapi import FastAPI
+from utils import parse_template_data, find_matching_template, guess_field_types
+
+db = TinyDB("./database/forms.json")
+app = FastAPI()
+
+templates = db.all()
+
+
+@app.post("/get_form")
+async def get_form(template_data: str):
+    template_data = parse_template_data(template_data)
+    template_name = find_matching_template(template_data, templates)
+    if template_name:
+        return {"template_name": template_name}
+    else:
+        field_types = guess_field_types(template_data)
+        return field_types
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        app,
+        host="127.0.0.2",
+        port=8001,
+    )
